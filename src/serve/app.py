8cee5850e80f987cc116a409d770d7ed218a3c2d
LiparAljaz <aljaz.lipar1@student.um.si>
2023-05-16 11:16:56 +0200

    mongo added

--- src/serve/app.py
+++ src/serve/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, request
 from flask_cors import CORS
 import requests
+from bson import json_util
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
 CORS(app)
 
 #python -m flask --debug run
+
+#add mongo connection
+
+client = MongoClient('mongodb+srv://test123:test123@cluster0.zy8ouhh.mongodb.net/?retryWrites=true&w=majority')
+db = client.IISprojekt
+
+zivali = db.zivali
 
 
 API_URL = "https://api-inference.huggingface.co/models/devMinty/iis-pet-classifier"
@@ -26,6 +37,14 @@ def predict():
 
     response = requests.post(API_URL, headers=headers, data=image_bytes)
     return response.json()
+
+@app.route('/zivali', methods=['POST'])
+def add_zivali():
+
+
+    #zivali.insert_one({'image': image_bytes,'prediction':})
+    return null
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
