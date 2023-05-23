5b0eb99aa84be8e1c4a9f8ff3970fbe9298156a1
LiparAljaz <aljaz.lipar1@student.um.si>
2023-05-23 09:54:40 +0200

    update api

--- src/serve/app.py
+++ src/serve/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import requests
 from pymongo import MongoClient
 from datetime import datetime
-#import openai
+import openai
 from bs4 import BeautifulSoup
 
 #python -m flask --debug run
@@ -15,7 +15,8 @@ zivali = db.zivali
 
 API_URL = "https://api-inference.huggingface.co/models/devMinty/iis-pet-classifier"
 headers = {"Authorization": "Bearer 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"}
-#openai.api_key = "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"
+
+openai.api_key = "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"
 
 #endregion
 
@@ -23,18 +24,18 @@ app = Flask(__name__)
 CORS(app)
 
 
-# def generate_anwser(question):
-#     responce = openai.Completion.create(
-#         engine = "text-ada-001",
-#         prompt = question,
-#         max_tokens = 1024,
-#         temperature = 0.7,
-#         n=1,
-#         stop = None,
-#         timeout = 10,
-#     )
-#     anwser = responce.choices[0].text.strip()
-#     return anwser
+def generate_anwser(question):
+    responce = openai.Completion.create(
+        engine = "text-ada-001",
+        prompt = question,
+        max_tokens = 1024,
+        temperature = 0.7,
+        n=1,
+        stop = None,
+        timeout = 10,
+    )
+    anwser = responce.choices[0].text.strip()
+    return anwser
 
 
 
@@ -74,62 +75,64 @@ def add_feedback():
 
     return "Thank you!", 201
 
-# @app.route('/aitest', methods=['POST'])
-# def aitest():
-#     question = request.form['question']
-#     anwser = generate_anwser(question)
-#     return jsonify({'anwser': anwser})
 
 
-@app.route('/wikitest/<animal>', methods=['POST'])
-def wikitest(animal):
-    url = "https://en.wikipedia.org/wiki/" + animal
-    if requests.get(url).status_code != 200:
-        return jsonify({'paragraph': "No information found for used search parameter"})
-    else:
-        response = requests.get("https://en.wikipedia.org/wiki/" + animal)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        paragraphs = soup.find_all('p')
+@app.route('/aitest/<question>', methods=['POST'])
+def aitest(question):
+    q = "Tell me a fun fact about " + question
+    anwser = generate_anwser(q)
+    return jsonify({'anwser': anwser})
 
-        for paragraph in paragraphs:
-            if animal in paragraph.get_text():
-                paragraph_text = paragraph.get_text()
-                paragraph_text = paragraph_text.replace('\n', '')
-                paragraph_text = paragraph_text.replace('[1]', '')
-                paragraph_text = paragraph_text.replace('[2]', '')
-                paragraph_text = paragraph_text.replace('[3]', '')
-                paragraph_text = paragraph_text.replace('[4]', '')
-                paragraph_text = paragraph_text.replace('[5]', '')
-                paragraph_text = paragraph_text.replace('[6]', '')
-                break
-            elif "_" in animal:
-                wordeOne = animal.split("_")[0]
-                if wordeOne in paragraph.get_text():
-                    paragraph_text = paragraph.get_text()
-                    paragraph_text = paragraph_text.replace('\n', '')
-                    paragraph_text = paragraph_text.replace('[1]', '')
-                    paragraph_text = paragraph_text.replace('[2]', '')
-                    paragraph_text = paragraph_text.replace('[3]', '')
-                    paragraph_text = paragraph_text.replace('[4]', '')
-                    paragraph_text = paragraph_text.replace('[5]', '')
-                    paragraph_text = paragraph_text.replace('[6]', '')
-                    break
-            elif " " in animal:
-                wordeOne = animal.split(" ")[0]
-                if wordeOne in paragraph.get_text():
-                    paragraph_text = paragraph.get_text()
-                    paragraph_text = paragraph_text.replace('\n', '')
-                    paragraph_text = paragraph_text.replace('[1]', '')
-                    paragraph_text = paragraph_text.replace('[2]', '')
-                    paragraph_text = paragraph_text.replace('[3]', '')
-                    paragraph_text = paragraph_text.replace('[4]', '')
-                    paragraph_text = paragraph_text.replace('[5]', '')
-                    paragraph_text = paragraph_text.replace('[6]', '')
-                    break
-            else:
-                paragraph_text = "No information found"
+
+# @app.route('/wikitest/<animal>', methods=['POST'])
+# def wikitest(animal):
+#     url = "https://en.wikipedia.org/wiki/" + animal
+#     if requests.get(url).status_code != 200:
+#         return jsonify({'paragraph': "No information found for used search parameter"})
+#     else:
+#         response = requests.get("https://en.wikipedia.org/wiki/" + animal)
+#         soup = BeautifulSoup(response.content, 'html.parser')
+#         paragraphs = soup.find_all('p')
+
+#         for paragraph in paragraphs:
+#             if animal in paragraph.get_text():
+#                 paragraph_text = paragraph.get_text()
+#                 paragraph_text = paragraph_text.replace('\n', '')
+#                 paragraph_text = paragraph_text.replace('[1]', '')
+#                 paragraph_text = paragraph_text.replace('[2]', '')
+#                 paragraph_text = paragraph_text.replace('[3]', '')
+#                 paragraph_text = paragraph_text.replace('[4]', '')
+#                 paragraph_text = paragraph_text.replace('[5]', '')
+#                 paragraph_text = paragraph_text.replace('[6]', '')
+#                 break
+#             elif "_" in animal:
+#                 wordeOne = animal.split("_")[0]
+#                 if wordeOne in paragraph.get_text():
+#                     paragraph_text = paragraph.get_text()
+#                     paragraph_text = paragraph_text.replace('\n', '')
+#                     paragraph_text = paragraph_text.replace('[1]', '')
+#                     paragraph_text = paragraph_text.replace('[2]', '')
+#                     paragraph_text = paragraph_text.replace('[3]', '')
+#                     paragraph_text = paragraph_text.replace('[4]', '')
+#                     paragraph_text = paragraph_text.replace('[5]', '')
+#                     paragraph_text = paragraph_text.replace('[6]', '')
+#                     break
+#             elif " " in animal:
+#                 wordeOne = animal.split(" ")[0]
+#                 if wordeOne in paragraph.get_text():
+#                     paragraph_text = paragraph.get_text()
+#                     paragraph_text = paragraph_text.replace('\n', '')
+#                     paragraph_text = paragraph_text.replace('[1]', '')
+#                     paragraph_text = paragraph_text.replace('[2]', '')
+#                     paragraph_text = paragraph_text.replace('[3]', '')
+#                     paragraph_text = paragraph_text.replace('[4]', '')
+#                     paragraph_text = paragraph_text.replace('[5]', '')
+#                     paragraph_text = paragraph_text.replace('[6]', '')
+#                     break
+#             else:
+#                 paragraph_text = "No information found"
         
-        return jsonify({'paragraph': paragraph_text})
+#         return jsonify({'paragraph': paragraph_text})
 
 
 
