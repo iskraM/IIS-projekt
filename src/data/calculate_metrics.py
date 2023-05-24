6734c0bd13f05ba5ffe38a61d08aabbde7968466
Marcel Iskrač <marceliskra@gmail.com>
2023-05-24 22:15:25 +0200

    Daily metrics calculation

---
+++ src/data/calculate_metrics.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URL"))
+db = client.IISprojekt
+collection_zivali = db.zivali
+
+# get all entries from db
+all_entries = collection_zivali.find()
+
+# calculate percent of entries that have matching values of prediction and dejanska_zival
+correct = 0
+count = 0
+for entry in all_entries:
+    count += 1
+    if entry['true_label'] == entry['predicted_label']:
+        correct += 1
+    
+print(f"{correct} out of {count} are correct: {correct/count*100}%")
+
+
